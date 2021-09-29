f158a27e8da1ecc14fbcd2467d926fff1bca1dd3
moise10r <moiserushanika2006@gmail.com>
2021-09-29 17:36:37 +0200

    Add new tabables.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,33 +24,33 @@ CREATE TABLE invoices (
     PRIMARY KEY (id)
 );
 
--- CREATE TABLE medical_histories  (
---     id               INT GENERATED ALWAYS AS IDENTITY,
---     admitted_at      DATE, 
---     patient_id       INT,    
---     status           VARCHAR(100),
---     FOREIGN KEY (patient_id) REFERENCES patients(id),
---     PRIMARY KEY (id)
--- );
+CREATE TABLE medical_histories  (
+    id               INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at      DATE, 
+    patient_id       INT,    
+    status           VARCHAR(100),
+    FOREIGN KEY (patient_id) REFERENCES patients(id),
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE invoice_items(
---     id               INT GENERATED ALWAYS AS IDENTITY,
---     unit_price       DECIMAL,  
---     quantity         INT,
---     total_price      DECIMAL, 
---     invoice_id       INT,    
---     treatment_id     INT,            
---     FOREIGN KEY (invoice_id) REFERENCES invoices(id),
---     FOREIGN KEY (treatment_id) REFERENCES treatments(id),    
---     PRIMARY KEY (id)
--- );
+CREATE TABLE invoice_items(
+    id               INT GENERATED ALWAYS AS IDENTITY,
+    unit_price       DECIMAL,  
+    quantity         INT,
+    total_price      DECIMAL, 
+    invoice_id       INT,    
+    treatment_id     INT,            
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id),    
+    PRIMARY KEY (id)
+);
 
 
 
--- CREATE TABLE medical_history_treatments (
---     medical_history_id  INT,
---     treatment_id     INT,
---     FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
---     FOREIGN KEY (treatment_id) REFERENCES treatments (id),
---     PRIMARY KEY (medical_history_id, treatment_id)
--- );
+CREATE TABLE medical_history_treatments (
+    medical_history_id  INT,
+    treatment_id     INT,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id),
+    PRIMARY KEY (medical_history_id, treatment_id)
+);
